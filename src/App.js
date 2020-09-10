a92946da6d286fca68f5cf1b1bbdf62311ef1f7e
Tejas pandit <pandit.tejas2017@gmail.com>
2020-09-10 23:40:01 +0530

    1 commerce

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Homepage from "./hompage.component";
+
+function App() {
+  return (
+    <div className>
+<Homepage />
+    </div>
+  );
+}
+
+export default App;
