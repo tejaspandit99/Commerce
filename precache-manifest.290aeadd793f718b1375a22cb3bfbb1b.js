d76d19814cae38e4a2e19dc8ca94b2fb32040ba0
Tejas pandit <pandit.tejas2017@gmail.com>
2020-09-29 00:33:47 +0530

    Updates

---
+++ precache-manifest.290aeadd793f718b1375a22cb3bfbb1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb1808b807f6214fd41",
+    "url": "/commerce/static/css/main.e62b809d.chunk.css"
+  },
+  {
+    "revision": "dcb1808b807f6214fd41",
+    "url": "/commerce/static/js/main.5c4190cc.chunk.js"
+  },
+  {
+    "revision": "6128eb4b6f1738acae43",
+    "url": "/commerce/static/js/runtime~main.77bbd495.js"
+  },
+  {
+    "revision": "749df0506416a65abd74",
+    "url": "/commerce/static/js/2.40be7bdb.chunk.js"
+  },
+  {
+    "revision": "1e0a2008774f39b79801ae2425d41bb0",
+    "url": "/commerce/index.html"
+  }
+];
