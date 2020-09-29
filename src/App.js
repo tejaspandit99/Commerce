6d75f37c26d53fe628ff528e624a279684c43c0b
Tejas pandit <pandit.tejas2017@gmail.com>
2020-09-29 11:27:43 +0530

    Now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { execPath } from 'process';
+
 import React from 'react';
 import './App.css';
 import Homepage from "./Page/homepage.component";
 
-import {Route,Link} from "react-router-dom";
-import {Switch} from "react-router-dom"; 
+import {Route} from "react-router-dom";
+
 const Hatspage= (props) => (
   <div>
 <h1 style={{color:"red"}} >Homepage 
@@ -16,7 +16,7 @@ const Hatspage= (props) => (
 function App() {
  return(
   <div>
-  <Route  exact path="/" component={Homepage}></Route>
+  <Route exact path="/" component={Homepage}></Route>
   <Route path="/hats" component={Hatspage}></Route>
   
   </div>
